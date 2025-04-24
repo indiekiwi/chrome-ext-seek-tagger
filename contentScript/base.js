f85cafed03d4fc0be1e2b577a1d112d730d2459a
allen <dev@awu.me>
2025-04-24 22:15:10 +1200

    Support for seek au site

--- contentScript/base.js
+++ contentScript/base.js
@@ -1,118 +1,131 @@
-let jobLinks = document.evaluate(
-    "//a[contains(@href, '/job/')]",
-    document,
-    null,
-    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-    null
-);
+(() => {
+    let currentHref = window.location.href;
+    let isNZ = null;
+    if (currentHref.includes("seek.com.au")) {
+        isNZ = false;
+    } else if (currentHref.includes("seek.co.nz")) {
+        isNZ = true;
+    } else {
+        return;
+    }
+    console.log(`Seek Tagger running on: ${isNZ ? 'NZ' : 'AU'}...`);
 
-let seenJobIds = new Set();
-let jobContexts = new Map();
-const syncer = chrome.storage.sync;
+    let jobLinks = document.evaluate(
+        "//a[contains(@href, '/job/')]",
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    );
 
-function createHash(tagList) {
-    return tagList
-        .sort()
-        .join(',')
-        .split('')
-        .reduce((hash, char) => {
-            hash = ((hash << 5) - hash) + char.charCodeAt(0);
-            return hash & hash;
-        }, 0);
-}
+    let seenJobIds = new Set();
+    let jobContexts = new Map();
+    const syncer = chrome.storage.sync;
 
-function createCombinedHash(tagListHash, jobId) {
-    return (tagListHash + jobId)
-        .split('')
-        .reduce((hash, char) => {
-            hash = ((hash << 5) - hash) + char.charCodeAt(0);
-            return hash & hash;
-        }, 0);
-}
-
-function init(tagList) {
-    const tagListHash = createHash(tagList);
-
-    for (let i = 0; i < jobLinks.snapshotLength; i++) {
-        let context = jobLinks.snapshotItem(i);
-        let href = $(context).attr('href');
-        let jobIdMatch = href.match(/\/job\/(\d+)/);
-
-        if (jobIdMatch) {
-            let jobId = jobIdMatch[1];
-            seenJobIds.add(jobId);
-            jobContexts.set(jobId, context);
-        }
+    function createHash(tagList) {
+        return tagList
+            .sort()
+            .join(',')
+            .split('')
+            .reduce((hash, char) => {
+                hash = ((hash << 5) - hash) + char.charCodeAt(0);
+                return hash & hash;
+            }, 0);
     }
 
-    seenJobIds.forEach(jobId => {
-        let fullUrl = `https://www.seek.co.nz/job/${jobId}`;
-        let context = jobContexts.get(jobId);
-        let combinedHash = createCombinedHash(tagListHash, jobId);
+    function createCombinedHash(tagListHash, jobId) {
+        return (tagListHash + jobId)
+            .split('')
+            .reduce((hash, char) => {
+                hash = ((hash << 5) - hash) + char.charCodeAt(0);
+                return hash & hash;
+            }, 0);
+    }
 
-        syncer.get(jobId, (result) => {
-            if (result[jobId] && result[jobId].hash === combinedHash) {
-                sleep(500).then(() => {
-                    let cachedTags = result[jobId].tags.join(', ');
-                    let injectHTML = `<div style="margin: 5px; padding: 5px; background: greenyellow">${cachedTags}</div>`;
-                    $(injectHTML).insertAfter(context);
-                });
-            } else {
-                sleep(500).then(() => {
-                    $.ajax({
-                        url: fullUrl,
-                        type: "GET",
-                        success: function (response) {
-                            let foundTags = [];
-                            tagList.forEach(tag => {
-                                let regex = new RegExp(tag, 'i');
-                                if (regex.test(response)) {
-                                    foundTags.push(`#${tag}`);
-                                }
-                            });
+    function init(tagList) {
+        const tagListHash = createHash(tagList);
 
-                            let injectHTML = `<div style="margin: 5px; padding: 5px; background: greenyellow">${foundTags.join(', ')}</div>`;
-                            $(injectHTML).insertAfter(context);
+        for (let i = 0; i < jobLinks.snapshotLength; i++) {
+            let context = jobLinks.snapshotItem(i);
+            let href = $(context).attr('href');
+            let jobIdMatch = href.match(/\/job\/(\d+)/);
 
-                            let cacheData = {
-                                [jobId]: { hash: combinedHash, tags: foundTags }
-                            };
-                            syncer.set(cacheData);
-                        }
+            if (jobIdMatch) {
+                let jobId = jobIdMatch[1];
+                seenJobIds.add(jobId);
+                jobContexts.set(jobId, context);
+            }
+        }
+
+        seenJobIds.forEach(jobId => {
+            let fullUrl = isNZ ? `https://www.seek.co.nz/job/${jobId}` : `https://www.seek.com.au/job/${jobId}`;
+            let context = jobContexts.get(jobId);
+            let combinedHash = createCombinedHash(tagListHash, jobId);
+
+            syncer.get(jobId, (result) => {
+                if (result[jobId] && result[jobId].hash === combinedHash) {
+                    sleep(500).then(() => {
+                        let cachedTags = result[jobId].tags.join(', ');
+                        let injectHTML = `<div style="margin: 5px; padding: 5px; background: greenyellow">${cachedTags}</div>`;
+                        $(injectHTML).insertAfter(context);
                     });
-                });
+                } else {
+                    sleep(500).then(() => {
+                        $.ajax({
+                            url: fullUrl,
+                            type: "GET",
+                            success: function (response) {
+                                let foundTags = [];
+                                tagList.forEach(tag => {
+                                    let regex = new RegExp(tag, 'i');
+                                    if (regex.test(response)) {
+                                        foundTags.push(`#${tag}`);
+                                    }
+                                });
+
+                                let injectHTML = `<div style="margin: 5px; padding: 5px; background: greenyellow">${foundTags.join(', ')}</div>`;
+                                $(injectHTML).insertAfter(context);
+
+                                let cacheData = {
+                                    [jobId]: { hash: combinedHash, tags: foundTags }
+                                };
+                                syncer.set(cacheData);
+                            }
+                        });
+                    });
+                }
+            });
+        });
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function setupObserver(tagList) {
+        const observer = new MutationObserver(() => {
+            const currentJobLinks = document.evaluate(
+                "//a[contains(@href, '/job/')]",
+                document,
+                null,
+                XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+                null
+            );
+
+            if (currentJobLinks.snapshotLength !== jobLinks.snapshotLength) {
+                jobLinks = currentJobLinks;
+                seenJobIds.clear();
+                jobContexts.clear();
+                init(tagList);
             }
         });
+
+        observer.observe(document.body, { childList: true, subtree: true });
+    }
+
+    syncer.get('tagList', (data) => {
+        const tagList = data.tagList || [];
+        init(tagList);
+        setupObserver(tagList);
     });
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function setupObserver(tagList) {
-    const observer = new MutationObserver(() => {
-        const currentJobLinks = document.evaluate(
-            "//a[contains(@href, '/job/')]",
-            document,
-            null,
-            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-            null
-        );
-
-        if (currentJobLinks.snapshotLength !== jobLinks.snapshotLength) {
-            jobLinks = currentJobLinks;
-            seenJobIds.clear();
-            jobContexts.clear();
-            init(tagList);
-        }
-    });
-
-    observer.observe(document.body, { childList: true, subtree: true });
-}
-
-syncer.get('tagList', (data) => {
-    const tagList = data.tagList || [];
-    init(tagList);
-    setupObserver(tagList);
-});
+})();
